f7fbcf9bc590ee228fd3d457d1cd326d9987a9e2
SanchoPanza13 <wayan.el.sanchez13@gmail.com>
2025-02-21 16:29:39 +0100

    APARTADO WITCHTIME
    Apartado del slow motion hecho

--- Assets/Scripts/WitchTime.cs
+++ Assets/Scripts/WitchTime.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class WitchTime : MonoBehaviour
 {
-    // Update is called once per frame
+
+    public AudioClip witchTime;
+
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(Slowmo());
+            AudioManager.instance.PlayAudio(witchTime, "witchTimeAudio");
+        }
     }
+
+    IEnumerator Slowmo()
+    {
+        Time.timeScale = 0.25f;
+        yield return new WaitForSeconds(1.0f);
+        Time.timeScale = 1.0f;
+    }
+
+
+
+
+
 }
